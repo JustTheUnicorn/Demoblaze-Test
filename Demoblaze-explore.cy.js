9ab69930bc7219004e175dff0c34180b1055ad28
Mariana Soares Pires <42157640+JustTheUnicorn@users.noreply.github.com>
2023-06-06 16:37:15 -0300

    Update Demoblaze-explore.cy.js
    Viewport/Resolution fixed

--- Demoblaze-explore.cy.js
+++ Demoblaze-explore.cy.js
@@ -1,7 +1,14 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://www.demoblaze.com/index.html')
+       
+  cy.visit('https://www.demoblaze.com/index.html')
+  cy.viewport(1024, 768)
+  .wait(2000);
 
-    
+  cy.get('#cartur').click();
+
+  cy.get('.active > .nav-link').click();
+
+
   })
 })
