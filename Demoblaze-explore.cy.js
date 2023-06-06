41ab129c145b1df22fb61380b1f49aea8aa6e1cf
Mariana Soares Pires <42157640+JustTheUnicorn@users.noreply.github.com>
2023-06-06 16:56:40 -0300

    Update Demoblaze-explore.cy.js
    Cat adjustments, better understand of code

--- Demoblaze-explore.cy.js
+++ Demoblaze-explore.cy.js
@@ -1,14 +1,20 @@
 describe('template spec', () => {
   it('passes', () => {
-       
+    
+    
   cy.visit('https://www.demoblaze.com/index.html')
   cy.viewport(1024, 768)
   .wait(2000);
 
   cy.get('#cartur').click();
 
-  cy.get('.active > .nav-link').click();
+  cy.get('.active > .nav-link').click()
+  .wait(2000)
+  cy.get('[onclick="byCat(\'phone\')"]').click();
 
+  cy.get('.active > .nav-link').click()
+  .wait(2000)
+  cy.get('[onclick="byCat(\'notebook\')"]').click();
 
   })
 })
