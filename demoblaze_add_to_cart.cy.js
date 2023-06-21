aad3b9c092415abb6573c9e006b40b919b1bf920
Mariana Soares Pires <42157640+JustTheUnicorn@users.noreply.github.com>
2023-06-21 14:50:06 -0300

    Update demoblaze_add_to_cart.cy.js
    little adjustments and ready to gooo

--- demoblaze_add_to_cart.cy.js
+++ demoblaze_add_to_cart.cy.js
@@ -1,6 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
-
+    
+    
   cy.visit('https://www.demoblaze.com/index.html')
   cy.viewport(1024, 768)
   .wait(2000);
@@ -12,14 +13,61 @@ describe('template spec', () => {
   cy.get('.col-sm-12 > .btn').click()
   .wait(2000);
 
-  cy.get('#cartur').click();
+  cy.get('#cartur').click()
+  .wait(2000);
 
-  cy.get('.active > .nav-link').click();
+  cy.get('.col-lg-8')
+  .contains('Pic').should('be.visible');
 
-  cy.get('[onclick="byCat(\'notebook\')"]').click();
+  cy.get('thead > tr > :nth-child(2)')
+  .contains('Title').should('be.visible')
+
+  cy.get('.success > :nth-child(2)')
+  .contains('Sony xperia z5').should('be.visible');
+
+  cy.get('thead > tr > :nth-child(3)')
+  .contains('Price').should('be.visible');
+
+  cy.get('.success > :nth-child(3)')
+  .contains('320').should('be.visible');
+
+  cy.get('#totalp')
+  .contains('320').should('be.visible');
+
+  cy.get('.active > .nav-link').click()
+  .wait(2000);
+
+  cy.get('[onclick="byCat(\'notebook\')"]').click()
+  .wait(2000);
 
   cy.get(':nth-child(6) > .card > .card-block > .card-title > .hrefch')
   .contains('MacBook Pro').should('be.visible');
+
+  cy.get(':nth-child(6) > .card > .card-block > .card-title > .hrefch').click();
+
+  cy.get('.col-sm-12 > .btn').click();
+
+  cy.get('#cartur').click()
+  .wait(2000);
+
+  cy.get('.col-lg-8')
+  .contains('Pic').should('be.visible');
+
+  cy.get('thead > tr > :nth-child(2)')
+  .contains('Title').should('be.visible');
+
+  cy.get('#tbodyid > :nth-child(2) > :nth-child(2)')
+  .contains('MacBook Pro').should('be.visible');
+
+  cy.get('thead > tr > :nth-child(3)')
+  .contains('Price').should('be.visible');
+
+  cy.get('#tbodyid > :nth-child(1) > :nth-child(3)')
+
+  cy.get('#totalp')
+  .contains('1420').should('be.visible');
+
+  cy.get('.col-lg-1 > .btn').click();
 
   })
 })
