39a7ba6da532cfb3b0f8dadc3fdb5a813980b6a1
Mariana Soares Pires <42157640+JustTheUnicorn@users.noreply.github.com>
2023-06-21 11:15:24 -0300

    Update demoblaze_add_to_cart.cy.js
    add

--- demoblaze_add_to_cart.cy.js
+++ demoblaze_add_to_cart.cy.js
@@ -10,9 +10,17 @@ describe('template spec', () => {
   .contains('Sony xperia z5').should('be.visible')
   .click();
 
-  cy.get('.col-sm-12 > .btn').click();
+  cy.get('.col-sm-12 > .btn').click()
+  .wait(2000);
 
   cy.get('#cartur').click();
+
+  cy.get('.active > .nav-link').click();
+
+  cy.get('[onclick="byCat(\'notebook\')"]').click();
+
+  cy.get(':nth-child(6) > .card > .card-block > .card-title > .hrefch')
+  .contains('MacBook Pro').should('be.visible');
 
   })
 })
